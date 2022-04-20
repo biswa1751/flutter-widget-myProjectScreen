444761d29e5141dfb5cdcee6be6423aa2f50b7f2
Biswajit Behera <36480889+biswa1751@users.noreply.github.com>
2022-04-20 22:09:40 +0530

    my project widget

---
+++ lib/common_widgets/app_network_image.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:upwork_001_flutter_mpjs_screen/constants/theme/custom_project_theme.dart';
+
+class AppNetworkImage extends StatelessWidget {
+  final String? imageUrl;
+  final double width;
+  final double? height;
+  final String? text;
+  const AppNetworkImage({
+    Key? key,
+    this.imageUrl,
+    this.width = 80,
+    this.height = 80,
+    this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return imageUrl == null || (imageUrl?.isEmpty ?? true)
+        ? Container(
+            height: height,
+            width: width,
+            color: CustomProjectTheme.getCustomProjectThemeDefault()
+                .backgroundColor,
+          )
+        : Image.network(
+            imageUrl!,
+            height: height,
+            width: width,
+          );
+  }
+}
--- lib/common_widgets/my_project_widget.dart
+++ lib/common_widgets/my_project_widget.dart
@@ -1,11 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:upwork_001_flutter_mpjs_screen/common_widgets/app_network_image.dart';
 import 'package:upwork_001_flutter_mpjs_screen/features/feature_project_management/models/project.dart';
 
 class MyProjectWidget extends StatelessWidget {
-  const MyProjectWidget({Key? key, required this.projectModel}) : super(key: key);
+  const MyProjectWidget({Key? key, required this.projectModel})
+      : super(key: key);
   final ProjectModel projectModel;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final pages = projectModel.pages;
+    return Column(
+      children: <Widget>[
+        Card(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(projectModel.title),
+          ),
+        ),
+        AppNetworkImage(
+          imageUrl: pages.first?.imgUrl,
+          height: 300,
+          width: double.maxFinite,
+        ),
+        Row(
+          children: List.generate(
+              pages.length > 4 ? 4 : pages.length - 1,
+              (index) => Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: GestureDetector(
+                        onTap: () {
+                          debugPrint(pages[index]?.id.toString());
+                        },
+                        child: AppNetworkImage(
+                          imageUrl: pages[index + 1]?.imgUrl,
+                          height: 50,
+                        ),
+                      ),
+                    ),
+                  )),
+        )
+      ],
+    );
   }
 }
