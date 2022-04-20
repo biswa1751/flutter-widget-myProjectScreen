5bf3508c196dc95adec64bef630b7c8693f11ad1
Biswajit Behera <36480889+biswa1751@users.noreply.github.com>
2022-04-20 22:12:24 +0530

    theme floating action button

---
+++ lib/common_widgets/theme_floating_action_button.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:upwork_001_flutter_mpjs_screen/constants/theme/custom_project_theme.dart';
+
+class ThemeFloatingActionButton extends StatelessWidget {
+  const ThemeFloatingActionButton(
+      {Key? key, required this.onPressed, required this.child})
+      : super(key: key);
+  final VoidCallback onPressed;
+  final Widget child;
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: onPressed,
+      backgroundColor:
+          CustomProjectTheme.getCustomProjectThemeDefault().backgroundColor,
+      child: child,
+    );
+  }
+}
