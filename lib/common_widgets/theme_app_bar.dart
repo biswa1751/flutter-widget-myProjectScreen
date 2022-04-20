5d2a0b6af0f8947714baa68e55804d7b2ce984b1
Biswajit Behera <36480889+biswa1751@users.noreply.github.com>
2022-04-20 21:34:27 +0530

    add theme app bar

---
+++ lib/common_widgets/theme_app_bar.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:upwork_001_flutter_mpjs_screen/constants/theme/custom_project_theme.dart';
+
+class ThemeAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  const ThemeAppBar({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(title),
+      backgroundColor:
+          CustomProjectTheme.getCustomProjectThemeDefault().backgroundColor,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(54.0);
+}
